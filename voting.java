9bdaac8da8dd5b8d8ff6abbec7b7248bdce50405
dsanooj <142886987+dsanooj@users.noreply.github.com>
2023-08-22 09:55:15 +0000

    completed

---
+++ voting.java
@@ -0,0 +1,11 @@
+
+class voting{
+    public static void main(String args[]){
+        int a=17;
+        if(a<=18){
+         System.out.println("Not eligible");}
+         else{
+          System.out.println("ELigible");}
+
+    }
+}
