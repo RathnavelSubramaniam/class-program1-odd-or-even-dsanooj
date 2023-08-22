5b4119c3f1c8234c837c41c7d5281678483dd4b2
dsanooj <142886987+dsanooj@users.noreply.github.com>
2023-08-22 09:26:03 +0000

    completed

---
+++ oddeven.java
@@ -0,0 +1,14 @@
+class oddeven{
+    public static void main(String args[]){
+    int num=3;
+    if(num%2==0)
+    {System.out.println("The given number is EVEN");
+
+    }
+    else
+    {
+        System.out.println("The given number is ODD");
+    }
+
+}
+ }
